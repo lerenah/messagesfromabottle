ddd089707f4eaa31b5318816efc327945ab5cc13
Lerena Holloway <holloway.lerena@gmail.com>
2019-09-09 15:46:36 -0400

    updates

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client'));
 
   app.get('/*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client', '/public/index.html'));
+    response.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
