e8f68783400fa596629949dfc8b66e4c813abad8
Lerena Holloway <holloway.lerena@gmail.com>
2019-10-18 15:20:56 -0400

    adds 3 components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Index from './components/layout/Index';
+import Hosts from './components/layout/Hosts';
+import Message from './components/layout/Message';
+import Contact from './components/layout/Contact';
 import Navbar from './components/layout/Navbar';
 import Slider from './components/Slider';
 
@@ -16,6 +19,9 @@ class App extends React.Component {
           <div className='container'>
             <Switch>
               <Route exact path='/' component={Index} />
+              <Route exact path='/hosts' component={Hosts} />
+              <Route exact path='/message' component={Message} />
+              <Route exact path='/contact' component={Contact} />
             </Switch>
           </div>
         </React.Fragment>
