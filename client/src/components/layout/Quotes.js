31b2f64aa3b2e1403aa034d0dc4c25257f4461f8
Lerena Holloway <holloway.lerena@gmail.com>
2019-10-19 16:54:15 -0400

    updates quotes section

--- client/src/components/layout/Quotes.js
+++ client/src/components/layout/Quotes.js
@@ -2,43 +2,55 @@ import React from 'react';
 
 export default function Quotes() {
   return (
-    <div style={{ backgroundColor: '#fbeec1' }}>
-      <div className='container'>
+    <div style={{ backgroundColor: '#fcf9ed' }}>
+      <div className='container' style={{ padding: 20 }}>
         <div className='row'>
           <div className='col-sm'>
-            <img src='https://picsum.photos/100/100' alt='pic'></img>
-            <h4>- Victor Frankl</h4>
+            <img
+              src='assets/quotes1.jpg'
+              alt='pic'
+              style={{ height: 100, width: 100 }}
+            ></img>
+            <h4>William Blake</h4>
             <p>
-              "Between stimulus and response there is a space. In that space is
-              our power to choose our response. In our response lies our growth
-              and our freedom.
+              "Ability make take you to the top, but it takes character to stay
+              there."
             </p>
           </div>
           <div className='col-sm'>
-            <img src='https://picsum.photos/100/100' alt='pic'></img>
-            <h4>- Victor Frankl</h4>
+            <img
+              src='assets/quotes2.jpg'
+              alt='pic'
+              style={{ height: 100, width: 100 }}
+            ></img>
+            <h4>Bruce Lee</h4>
             <p>
-              "Between stimulus and response there is a space. In that space is
-              our power to choose our response. In our response lies our growth
-              and our freedom.
+              "Absorb what is useful, Discard what is not, Add what is uniquely
+              your own."
             </p>
           </div>
           <div className='col-sm'>
-            <img src='https://picsum.photos/100/100' alt='pic'></img>
-            <h4>- Victor Frankl</h4>
+            <img
+              src='assets/quotes3.jpg'
+              alt='pic'
+              style={{ height: 100, width: 100 }}
+            ></img>
+            <h4>George Bernard Shaw</h4>
             <p>
-              "Between stimulus and response there is a space. In that space is
-              our power to choose our response. In our response lies our growth
-              and our freedom.
+              "Life isn't about finding yourself. Life is about creating
+              yourself."
             </p>
           </div>
           <div className='col-sm'>
-            <img src='https://picsum.photos/100/100' alt='pic'></img>
-            <h4>- Victor Frankl</h4>
+            <img
+              src='assets/quotes4.jpg'
+              alt='pic'
+              style={{ height: 100, width: 100 }}
+            ></img>
+            <h4>Confucius</h4>
             <p>
-              "Between stimulus and response there is a space. In that space is
-              our power to choose our response. In our response lies our growth
-              and our freedom.
+              "They must often cahnge who would be constant in happiness or
+              wisdom."
             </p>
           </div>
         </div>
