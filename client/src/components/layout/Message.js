1134eaeb239678c2eb6aff34da14505fcc11723b
enodi <enodiaudu5@gmail.com>
2020-08-03 12:30:16 +0100

    make navbar responsive

--- client/src/components/layout/Message.js
+++ client/src/components/layout/Message.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import SingleMessage from './SingleMessage';
-import Spinner from './Spinner';
-import { Row, Button, Container, Jumbotron } from 'react-bootstrap';
+import SingleMessage from "./SingleMessage";
+import Spinner from "./Spinner";
+import { Row, Button, Container, Jumbotron } from "react-bootstrap";
 
 class Message extends React.Component {
   constructor() {
@@ -15,12 +15,12 @@ class Message extends React.Component {
 
   async componentDidMount() {
     try {
-      let { data } = await axios.get('/api/message');
-      console.log(data, ' is DATA');
+      let { data } = await axios.get("/api/message");
+      console.log(data, " is DATA");
       this.setState({ message_list: data });
-      console.log(this.state.message_list, ' is LIST');
+      console.log(this.state.message_list, " is LIST");
     } catch (err) {
-      console.log(err, ' is err');
+      console.log(err, " is err");
     }
   }
 
@@ -28,20 +28,20 @@ class Message extends React.Component {
     return async () => {
       const { data } = await axios.get(`/api/message/${messageId}`);
       this.setState({
-        selectedMessage: data
+        selectedMessage: data,
       });
     };
   }
   deselectMessage() {
     this.setState({
-      selectedMessage: {}
+      selectedMessage: {},
     });
   }
 
   render() {
     const { message_list } = this.state;
     return (
-      <div className='container'>
+      <div className="container">
         {message_list.length === 0 || message_list.length === undefined ? (
           <Spinner></Spinner>
         ) : (
@@ -50,10 +50,10 @@ class Message extends React.Component {
               <Jumbotron>
                 <h2>What's Your Message for the Bottle</h2>
                 <p>Leave us a message, share your thoughts here!</p>
-                <Button bsStyle='primary'>Share Your Message</Button>
+                <Button bsStyle="primary">Share Your Message</Button>
               </Jumbotron>
-              <Row className='show-grid text-center'>
-                {message_list.map(message => {
+              <Row className="show-grid text-center">
+                {message_list.map((message) => {
                   return (
                     <SingleMessage
                       key={message.id}
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,71 +5,70 @@ const NavBar = () => (
   <Navbar
     activekey="/"
     onSelect={(selectedKey) => console.log(`selected ${selectedKey}`)}
-    className="justify-content-between navigation navbar navbar-expand-md"
-    style={{
-      backgroundColor: "rgb(79, 73, 137)",
-      marginBottom: 0,
-      zIndex: 9,
-      width: "100vw",
-    }}
+    collapseOnSelect
+    expand="lg"
+    className="navigation navbar navbar-expand-md navbar-header"
   >
-    <Nav.Item className="nav-item">
-      <Nav.Link
-        className="navbar-brand mb-4 nav-link"
-        href="https://podcasts.apple.com/us/podcast/messages-from-a-bottle-podcast/id1340730783"
-        target="_blank"
-      >
-        <img
-          src="assets/cropped-MFAB-final-art-cover.jpg"
-          width="40"
-          height="40"
-          className="d-inline-block align-top logo"
-          alt="logo"
-        />
-      </Nav.Link>
-    </Nav.Item>
-
-    <Nav.Item className="nav-item">
-      <Nav.Link className="nav-link" eventKey="/" href="/">
-        Home
-      </Nav.Link>
-    </Nav.Item>
-
-    <Nav.Item className="nav-item">
-      <Nav.Link
-        className="nav-link"
-        eventKey="/allpodcasts"
-        href="/allpodcasts"
-      >
-        Podcast Episodes
-      </Nav.Link>
-    </Nav.Item>
-
-    <Nav.Item className="nav-item">
-      <Nav.Link className="nav-link" eventKey="/hosts" href="/hosts">
-        Meet Your Hosts
-      </Nav.Link>
-    </Nav.Item>
-
-    <Nav.Item className="nav-item">
-      <Nav.Link className="nav-link" eventKey="/message" href="/message">
-        Share Your Message
-      </Nav.Link>
-    </Nav.Item>
-    <Nav.Item className="nav-item">
-      <Nav.Link className="nav-link" eventKey="/contact" href="/contact">
-        Contact Us
-      </Nav.Link>
-    </Nav.Item>
-    <Nav.Item id="subscribe" className="nav-item">
-      <Nav.Link
-        href="https://podcasts.apple.com/us/podcast/messages-from-a-bottle-podcast/id1340730783"
-        target="_blank"
-        className="btn btn-info nav-link"
-      >
-        Subscribe
-      </Nav.Link>
-    </Nav.Item>
+    <Navbar.Brand
+      className="navbar-brand mb-4 nav-link"
+      href="https://podcasts.apple.com/us/podcast/messages-from-a-bottle-podcast/id1340730783"
+      target="_blank"
+    >
+      <img
+        src="assets/cropped-MFAB-final-art-cover.jpg"
+        width="40"
+        height="40"
+        className="d-inline-block align-top logo"
+        alt="logo"
+      />
+    </Navbar.Brand>
+    <Navbar.Toggle
+      aria-controls="responsive-navbar-nav"
+      className="navbar-header__toggle-button"
+    />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto navbar-header__list">
+        <Nav.Link href="/" className="navbar-header__link" eventKey="/">
+          Home
+        </Nav.Link>
+        <Nav.Link
+          href="/allpodcasts"
+          className="navbar-header__link"
+          eventKey="/allpodcasts"
+        >
+          Podcast Episodes
+        </Nav.Link>
+        <Nav.Link
+          href="/hosts"
+          className="navbar-header__link"
+          eventKey="/hosts"
+        >
+          Meet Your Hosts
+        </Nav.Link>
+        <Nav.Link
+          href="/message"
+          className="navbar-header__link"
+          eventKey="/message"
+        >
+          Share Your Message
+        </Nav.Link>
+        <Nav.Link
+          href="/contact"
+          className="navbar-header__link"
+          eventKey="/contact"
+        >
+          Contact Us
+        </Nav.Link>
+        <Nav.Link
+          id="subscribe"
+          className="btn btn-info subscribe-link "
+          href="https://podcasts.apple.com/us/podcast/messages-from-a-bottle-podcast/id1340730783"
+          target="_blank"
+        >
+          Subscribe
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
 
